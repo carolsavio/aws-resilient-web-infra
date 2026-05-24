2db942ce5f4c195ecd984b8925f73a3b3219d379
Carol Savio <aploriart.com>
2026-05-24 13:28:07 -0300

    add role and ssm

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -19,3 +19,21 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# IAM Role para EC2
+resource "aws_iam_role" "ec2_role" {
+  name = "Lab-EC2-Execution-Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+# Política SSM
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
